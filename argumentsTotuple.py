0ba0f1ce30113ff8133188d88be70ced6ec30638
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 12:05:16 +0300

    integer arguments to tuple

--- argumentsTotuple.py
+++ argumentsTotuple.py
@@ -1,1 +1,10 @@
 # take three positional arguments and return them as a tuple
+
+def argument_t0_tuple(num1, num2, num3):
+    return (num1, num2, num3)
+
+num1 = int(input("Enter any number: "))
+num2 = int(input("Enter any number: "))
+num3 = int(input("Enter any number: "))
+
+print(argument_t0_tuple(num1, num2, num3))
