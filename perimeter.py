65a4b65b31bc9c78ccd498d8c7ebae0ebd6f9eb8
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 11:24:39 +0300

    perimeter of a quadrilateral

--- perimeter.py
+++ perimeter.py
@@ -1,1 +1,11 @@
 # perimeter of a quadrilateral
+
+def perimeter(length, width):
+    
+    return  2 * (length + width)
+length = float(input("Enter the length:"))
+width = float(input("Enter the width:"))
+
+result = perimeter(length, width)
+
+print("The perimeter of a quadrilateral is:", result)
