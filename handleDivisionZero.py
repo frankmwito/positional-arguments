53615fb3e7ea9d124a97a473c8bab168237a954a
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 11:42:26 +0300

    handle division by zero from two integers

--- handleDivisionZero.py
+++ handleDivisionZero.py
@@ -1,1 +1,15 @@
 # return the result of dividing two numbers and also handle division by zero
+
+def division(num1, num2):
+    if num1 or num2 == 0:
+        return "Error: Division by zero is not allowed"
+    else:
+        result = num1 / num2
+        return result
+    
+num1 = float(input("Enter any number: "))
+num2 = float(input("Enter any number: "))
+
+answer = division(num1, num2)
+
+print("The answer for division is", answer)
