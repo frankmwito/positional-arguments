4232a39458d7a76b9656b0b4aaaba9de3a633f09
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 11:52:01 +0300

    person's name arguments

--- nameArguments.py
+++ nameArguments.py
@@ -1,1 +1,9 @@
 # introduce a person using two names
+
+def persons_name(first_name, second_name):
+    return f"{first_name} {second_name}"
+
+first_name = input("Enter your forst name: ")
+second_name = input("Enter your second name: ")
+
+print(persons_name(first_name, second_name))
